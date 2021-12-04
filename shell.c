6abd9f23fb912f7141771ef3ae93d04233eb72aa
Rahul-Sharma-1234 <rahuls@cpp.edu>
2021-12-03 23:32:55 -0800

    Update shell.c

--- shell.c
+++ shell.c
@@ -1,3 +1,18 @@
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+/*
+  Function Declarations for builtin shell commands:
+ */
+int lsh_cd(char **args);
+int lsh_help(char **args);
+int lsh_exit(char **args);
+
+
 /*
   List of builtin commands, followed by their corresponding functions.
  */
