0fb8bcefc40c4dd1e556c8f59adc81fd49ad14a3
Rahul-Sharma-1234 <rahuls@cpp.edu>
2021-12-03 23:28:10 -0800

    Update shell.c

--- shell.c
+++ shell.c
@@ -1,3 +1,28 @@
+int lsh_launch(char **args)
+{
+  pid_t pid;
+  int status;
+
+  pid = fork();
+  if (pid == 0) {
+    // Child process
+    if (execvp(args[0], args) == -1) {
+      perror("lsh");
+    }
+    exit(EXIT_FAILURE);
+  } else if (pid < 0) {
+    // Error forking
+    perror("lsh");
+  } else {
+    // Parent process
+    do {
+      waitpid(pid, &status, WUNTRACED);
+    } while (!WIFEXITED(status) && !WIFSIGNALED(status));
+  }
+
+  return 1;
+}
+
 /**
    @brief Read a line of input from stdin.
    @return The line from stdin.
