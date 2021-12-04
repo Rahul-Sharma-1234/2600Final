1bb056b1d646b61a63943ed0e93684cf9fc6559d
Rahul-Sharma-1234 <rahuls@cpp.edu>
2021-12-03 23:32:03 -0800

    Update shell.c

--- shell.c
+++ shell.c
@@ -1,4 +1,21 @@
 /**
+   @brief Bultin command: change directory.
+   @param args List of args.  args[0] is "cd".  args[1] is the directory.
+   @return Always returns 1, to continue executing.
+ */
+int lsh_cd(char **args)
+{
+  if (args[1] == NULL) {
+    fprintf(stderr, "lsh: expected argument to \"cd\"\n");
+  } else {
+    if (chdir(args[1]) != 0) {
+      perror("lsh");
+    }
+  }
+  return 1;
+}
+
+/**
    @brief Builtin command: print help.
    @param args List of args.  Not examined.
    @return Always returns 1, to continue executing.
