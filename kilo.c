97f66fe881d25da86051c806f25bc0f1a560f528
Rahul-Sharma-1234 <rahuls@cpp.edu>
2021-12-10 20:10:59 -0800

    Update kilo.c

--- kilo.c
+++ kilo.c
@@ -55,6 +55,17 @@ enum editorHighlight {
 
 /*** data ***/
 
+struct editorSyntax {
+  char *filetype;
+  char **filematch;
+  char **keywords;
+  char *singleline_comment_start;
+  char *multiline_comment_start;
+  char *multiline_comment_end;
+  int flags;
+};
+
+
 struct editorConfig {
   int cx, cy;
 
