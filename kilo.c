4d205a82fb429ace69becb76a6f652fc7c13d73b
Rahul-Sharma-1234 <rahuls@cpp.edu>
2021-12-10 19:40:39 -0800

    Update kilo.c

--- kilo.c
+++ kilo.c
@@ -1,3 +1,5 @@
+/*** includes ***/
+
 #include <ctype.h>
 #include <errno.h>
 #include <stdio.h>
@@ -5,8 +7,12 @@
 #include <termios.h>
 #include <unistd.h>
 
+/*** data ***/
+
 
 struct termios orig_termios;
+
+/*** terminal ***/
 
 void die(const char *s) {
   perror(s);
@@ -29,6 +35,9 @@ void enableRawMode() {
   raw.c_cc[VTIME] = 1;
   if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) die("tcsetattr");
 }
+
+/*** init ***/
+
 int main() {
   enableRawMode();
   while (1) {
