5c33b51e805a6c317da402e3880ffc0127c497f3
Rahul-Sharma-1234 <rahuls@cpp.edu>
2021-12-10 20:25:50 -0800

    Update kilo.c

--- kilo.c
+++ kilo.c
@@ -573,6 +573,37 @@ void editorOpen(char *filename) {
   E.dirty = 0;
 }
 
+void editorSave() {
+  if (E.filename == NULL) {
+    E.filename = editorPrompt("Save as: %s (ESC to cancel)", NULL);
+    if (E.filename == NULL) {
+      editorSetStatusMessage("Save aborted");
+      return;
+    }
+    editorSelectSyntaxHighlight();
+  }
+  
+  int len;
+  char *buf = editorRowsToString(&len);
+
+  int fd = open(E.filename, O_RDWR | O_CREAT, 0644);
+  if (fd != -1) {
+    if (ftruncate(fd, len) != -1) {
+      if (write(fd, buf, len) == len) {
+        close(fd);
+        free(buf);
+        E.dirty = 0;
+        editorSetStatusMessage("%d bytes written to disk", len);
+        return;
+      }
+    }
+    close(fd);
+  }
+
+  free(buf);
+  editorSetStatusMessage("Can't save! I/O error: %s", strerror(errno));
+}
+
 /*** append buffer ***/
 struct abuf {
   char *b;
