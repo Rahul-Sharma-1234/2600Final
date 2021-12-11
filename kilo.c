cb09a8dd5d189524d4d8a4e0eb12e771959154b8
Rahul-Sharma-1234 <rahuls@cpp.edu>
2021-12-10 20:23:18 -0800

    Update kilo.c

--- kilo.c
+++ kilo.c
@@ -495,6 +495,20 @@ void editorInsertChar(int c) {
   editorRowInsertChar(&E.row[E.cy], E.cx, c);
   E.cx++;
 }
+void editorInsertNewline() {
+  if (E.cx == 0) {
+    editorInsertRow(E.cy, "", 0);
+  } else {
+    erow *row = &E.row[E.cy];
+    editorInsertRow(E.cy + 1, &row->chars[E.cx], row->size - E.cx);
+    row = &E.row[E.cy];
+    row->size = E.cx;
+    row->chars[row->size] = '\0';
+    editorUpdateRow(row);
+  }
+  E.cy++;
+  E.cx = 0;
+}
 
 
 /*** append buffer ***/
