6cfdf9e57586f4a42e6aa9acd7ea549a9c4d3bea
Rahul-Sharma-1234 <rahuls@cpp.edu>
2021-12-10 20:02:09 -0800

    Update kilo.c

--- kilo.c
+++ kilo.c
@@ -20,8 +20,8 @@
 
 struct editorConfig {
   int cx, cy;
-  
-    
+
+
   int screenrows;
   int screencols;
 
@@ -153,12 +153,12 @@ void editorDrawRows(struct abuf *ab) {
 
 void editorRefreshScreen() {
   struct abuf ab = ABUF_INIT;
-  
   abAppend(&ab, "\x1b[?25l", 6);
-  
   abAppend(&ab, "\x1b[H", 3);
   editorDrawRows(&ab);
-  abAppend(&ab, "\x1b[H", 3);
+  char buf[32];
+  snprintf(buf, sizeof(buf), "\x1b[%d;%dH", E.cy + 1, E.cx + 1);
+  abAppend(&ab, buf, strlen(buf));
   abAppend(&ab, "\x1b[?25h", 6);
   write(STDOUT_FILENO, ab.b, ab.len);
   abFree(&ab);
