7dbd4b0d08befc9b0bd1ab1d76dd8b7c75ce15bf
Rahul-Sharma-1234 <rahuls@cpp.edu>
2021-12-10 20:11:22 -0800

    Update kilo.c

--- kilo.c
+++ kilo.c
@@ -77,6 +77,23 @@ typedef struct erow {
 
 struct editorConfig {
   int cx, cy;
+  int rx;
+  int rowoff;
+  int coloff;
+  int screenrows; 
+  int screencols;
+  int numrows;
+  erow *row;
+  int dirty;
+  char *filename;
+  char statusmsg[80];
+  time_t statusmsg_time;
+  struct editorSyntax *syntax;
+  struct termios orig_termios;
+};
+
+struct editorConfig {
+  int cx, cy;
 
 
   int screenrows;
