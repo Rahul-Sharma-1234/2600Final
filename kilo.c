65d8310576f3054f1d539038dd988dd1b0c1bff1
Rahul-Sharma-1234 <rahuls@cpp.edu>
2021-12-10 20:03:34 -0800

    Update kilo.c

--- kilo.c
+++ kilo.c
@@ -14,7 +14,12 @@
 #define KILO_VERSION "0.0.1"
 #define CTRL_KEY(k) ((k) & 0x1f)
 
-
+enum editorKey {
+  ARROW_LEFT = 'a',
+  ARROW_RIGHT = 'd',
+  ARROW_UP = 'w',
+  ARROW_DOWN = 's'
+};
 
 /*** data ***/
 
