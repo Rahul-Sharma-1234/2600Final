d3c5705cc763f9f78f6f1bc2291056673054c62c
Rahul-Sharma-1234 <rahuls@cpp.edu>
2021-12-10 19:56:50 -0800

    Update kilo.c

--- kilo.c
+++ kilo.c
@@ -121,14 +121,23 @@ void editorProcessKeypress() {
 void editorDrawRows(struct abuf *ab) {
   int y;
   for (y = 0; y < E.screenrows; y++) {
-    abAppend(ab, "~", 1);
-
+    if (y == E.screenrows / 3) {
+      char welcome[80];
+      int welcomelen = snprintf(welcome, sizeof(welcome),
+        "Kilo editor -- version %s", KILO_VERSION);
+      if (welcomelen > E.screencols) welcomelen = E.screencols;
+      abAppend(ab, welcome, welcomelen);
+    } else {
+      abAppend(ab, "~", 1);
+    }
     abAppend(ab, "\x1b[K", 3);
     if (y < E.screenrows - 1) {
       abAppend(ab, "\r\n", 2);
     }
   }
 }
+
+
 void editorRefreshScreen() {
   struct abuf ab = ABUF_INIT;
   
