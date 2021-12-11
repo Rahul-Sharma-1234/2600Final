389505923404325341c7603d977bb7e583b851b2
Rahul-Sharma-1234 <rahuls@cpp.edu>
2021-12-10 20:12:51 -0800

    Update kilo.c

--- kilo.c
+++ kilo.c
@@ -92,16 +92,6 @@ struct editorConfig {
   struct termios orig_termios;
 };
 
-struct editorConfig {
-  int cx, cy;
-
-
-  int screenrows;
-  int screencols;
-
-  struct termios orig_termios;
-};
-struct editorConfig E;
 
 /*** terminal ***/
 
