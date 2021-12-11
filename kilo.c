65e757d20318c8b76cd22801c59f1fd465f0115f
Rahul-Sharma-1234 <rahuls@cpp.edu>
2021-12-10 19:45:10 -0800

    Update kilo.c

--- kilo.c
+++ kilo.c
@@ -64,6 +64,16 @@ void editorProcessKeypress() {
 void editorRefreshScreen() {
   write(STDOUT_FILENO, "\x1b[2J", 4);
   write(STDOUT_FILENO, "\x1b[H", 3);
+
+  editorDrawRows();
+  write(STDOUT_FILENO, "\x1b[H", 3);
+}
+
+void editorDrawRows() {
+  int y;
+  for (y = 0; y < 24; y++) {
+    write(STDOUT_FILENO, "~\r\n", 3);
+  }
 }
 
 
