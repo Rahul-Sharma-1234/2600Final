7070c044e87010ad108912e93d1ed5ffbae3a8a4
Rahul-Sharma-1234 <rahuls@cpp.edu>
2021-12-10 20:24:02 -0800

    Update kilo.c

--- kilo.c
+++ kilo.c
@@ -551,6 +551,25 @@ void editorOpen(char *filename) {
   free(E.filename);
   E.filename = strdup(filename);
   
+   editorSelectSyntaxHighlight();
+
+
+  FILE *fp = fopen(filename, "r");
+  if (!fp) die("fopen");
+
+  char *line = NULL;
+  size_t linecap = 0;
+  ssize_t linelen;
+  while ((linelen = getline(&line, &linecap, fp)) != -1) {
+    while (linelen > 0 && (line[linelen - 1] == '\n' ||
+                           line[linelen - 1] == '\r'))
+      linelen--;
+    editorInsertRow(E.numrows, line, linelen);
+  }
+  free(line);
+  fclose(fp);
+  E.dirty = 0;
+}
 
 /*** append buffer ***/
 struct abuf {
