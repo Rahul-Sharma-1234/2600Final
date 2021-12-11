e07e4e52a7ac3ed7a74694fa732ffdd343c68b8b
Rahul-Sharma-1234 <rahuls@cpp.edu>
2021-12-10 20:07:35 -0800

    Update kilo.c

--- kilo.c
+++ kilo.c
@@ -216,11 +216,23 @@ void editorRefreshScreen() {
 
 
 /*** init ***/
+
 void initEditor() {
-E.cx = 0;
-E.cy = 0;
+  E.cx = 0;
+  E.cy = 0;
+  E.rx = 0;
+  E.rowoff = 0;
+  E.coloff = 0;
+  E.numrows = 0;
+  E.row = NULL;
+  E.dirty = 0;
+  E.filename = NULL;
+  E.statusmsg[0] = '\0';
+  E.statusmsg_time = 0;
+  E.syntax = NULL;
 
   if (getWindowSize(&E.screenrows, &E.screencols) == -1) die("getWindowSize");
+  E.screenrows -= 2;
 }
 
 int main(int argc, char *argv[]) {
