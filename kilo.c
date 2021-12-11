6bae4ecaebbe202002631f6e05920ab1eeb90149
Rahul-Sharma-1234 <rahuls@cpp.edu>
2021-12-10 20:23:26 -0800

    Update kilo.c

--- kilo.c
+++ kilo.c
@@ -510,6 +510,23 @@ void editorInsertNewline() {
   E.cx = 0;
 }
 
+void editorDelChar() {
+  if (E.cy == E.numrows) return;
+  if (E.cx == 0 && E.cy == 0) return;
+
+
+  erow *row = &E.row[E.cy];
+  if (E.cx > 0) {
+    editorRowDelChar(row, E.cx - 1);
+    E.cx--;
+    } else {
+    E.cx = E.row[E.cy - 1].size;
+    editorRowAppendString(&E.row[E.cy - 1], row->chars, row->size);
+    editorDelRow(E.cy);
+    E.cy--;
+  }
+}
+
 
 /*** append buffer ***/
 struct abuf {
