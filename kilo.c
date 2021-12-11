e11136998cb7cf27b061f3c3e9e59bfd4dc3930d
Rahul-Sharma-1234 <rahuls@cpp.edu>
2021-12-10 19:39:42 -0800

    Update kilo.c

--- kilo.c
+++ kilo.c
@@ -1,6 +1,11 @@
+#include <ctype.h>
+#include <errno.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <termios.h>
 #include <unistd.h>
+
+
 struct termios orig_termios;
 
 void die(const char *s) {
