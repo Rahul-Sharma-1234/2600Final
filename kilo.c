551bd0c6c3fef177fe7bea9179ab0626c063a95c
Rahul-Sharma-1234 <rahuls@cpp.edu>
2021-12-10 20:21:55 -0800

    Update kilo.c

--- kilo.c
+++ kilo.c
@@ -457,6 +457,16 @@ void editorDelRow(int at) {
   E.dirty++;
 }
 
+void editorRowInsertChar(erow *row, int at, int c) {
+  if (at < 0 || at > row->size) at = row->size;
+  row->chars = realloc(row->chars, row->size + 2);
+  memmove(&row->chars[at + 1], &row->chars[at], row->size - at + 1);
+  row->size++;
+  row->chars[at] = c;
+  editorUpdateRow(row);
+  E.dirty++;
+}
+
 /*** append buffer ***/
 struct abuf {
   char *b;
