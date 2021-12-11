6d91d4b6407c2715c2a1cb9cafb94647338d2807
Rahul-Sharma-1234 <rahuls@cpp.edu>
2021-12-10 20:02:39 -0800

    Update kilo.c

--- kilo.c
+++ kilo.c
@@ -116,6 +116,24 @@ void abFree(struct abuf *ab) {
 
 
 /*** input ***/
+
+void editorMoveCursor(char key) {
+  switch (key) {
+    case 'a':
+      E.cx--;
+      break;
+    case 'd':
+      E.cx++;
+      break;
+    case 'w':
+      E.cy--;
+      break;
+    case 's':
+      E.cy++;
+      break;
+  }
+}
+
 void editorProcessKeypress() {
   char c = editorReadKey();
   switch (c) {
