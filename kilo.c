c67e42b7bc9b6a20bd9617fb2e733ade3f6b9de8
Rahul-Sharma-1234 <rahuls@cpp.edu>
2021-12-10 20:21:11 -0800

    Update kilo.c

--- kilo.c
+++ kilo.c
@@ -393,6 +393,31 @@ int editorRowRxToCx(erow *row, int rx) {
   return cx;
 }
 
+void editorUpdateRow(erow *row) {
+  int tabs = 0;
+  int j;
+  for (j = 0; j < row->size; j++)
+    if (row->chars[j] == '\t') tabs++;
+
+  free(row->render);
+  row->render = malloc(row->size + tabs*(KILO_TAB_STOP - 1) + 1);
+
+  int idx = 0;
+  for (j = 0; j < row->size; j++) {
+    if (row->chars[j] == '\t') {
+      row->render[idx++] = ' ';
+      while (idx % KILO_TAB_STOP != 0) row->render[idx++] = ' ';
+    } else {
+      row->render[idx++] = row->chars[j];
+    }
+  }
+  row->render[idx] = '\0';
+  row->rsize = idx;
+
+  editorUpdateSyntax(row);
+}
+
+
 /*** append buffer ***/
 struct abuf {
   char *b;
