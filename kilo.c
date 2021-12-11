3ff3e9d71d0d95db106e052869edab3814e648a1
Rahul-Sharma-1234 <rahuls@cpp.edu>
2021-12-10 20:02:53 -0800

    Update kilo.c

--- kilo.c
+++ kilo.c
@@ -138,7 +138,15 @@ void editorProcessKeypress() {
   char c = editorReadKey();
   switch (c) {
     case CTRL_KEY('q'):
+      write(STDOUT_FILENO, "\x1b[2J", 4);
+      write(STDOUT_FILENO, "\x1b[H", 3);
       exit(0);
+      break;
+    case 'w':
+    case 's':
+    case 'a':
+    case 'd':
+      editorMoveCursor(c);
       break;
   }
 }
