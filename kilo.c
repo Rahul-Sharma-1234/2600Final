ed2733142c170799279b5e3ef0ee76c274bfa763
Rahul-Sharma-1234 <rahuls@cpp.edu>
2021-12-10 19:37:27 -0800

    Update kilo.c

--- kilo.c
+++ kilo.c
@@ -13,6 +13,9 @@ void enableRawMode() {
   raw.c_oflag &= ~(OPOST);
   raw.c_cflag |= (CS8);
   raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
+  raw.c_cc[VMIN] = 0;
+  raw.c_cc[VTIME] = 1;
+  
   tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
 int main() {
