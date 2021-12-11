ff62c1c8cfc03b90375faf85b268a41614fa2af0
Rahul-Sharma-1234 <rahuls@cpp.edu>
2021-12-10 20:07:16 -0800

    Update kilo.c

--- kilo.c
+++ kilo.c
@@ -223,13 +223,21 @@ E.cy = 0;
   if (getWindowSize(&E.screenrows, &E.screencols) == -1) die("getWindowSize");
 }
 
-int main() {
+int main(int argc, char *argv[]) {
   enableRawMode();
   initEditor();
+  if (argc >= 2) {
+    editorOpen(argv[1]);
+  }
+
+
+  editorSetStatusMessage(
+    "HELP: Ctrl-S = save | Ctrl-Q = quit | Ctrl-F = find");
 
   while (1) {
-       editorRefreshScreen();
-      editorProcessKeypress();
+    editorRefreshScreen();
+    editorProcessKeypress();
   }
+
   return 0;
 }
